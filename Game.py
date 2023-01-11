b8b164c01acd075f1f9e9376295b84ee3195d55d
Drew Nelson <drew.nelson72@gmail.com>
2023-01-11 16:45:53 -0500

    fixed import issue

--- Game.py
+++ Game.py
@@ -1,22 +1,22 @@
-from Board import set_current_state, is_winner, is_draw, get_board
+from Board import Board
 
 class Game:
     def make_move(self, row, col, player):
         """Makes a move based on the given [row][col] position and which player is making a turn, 'x' or 'o'"""
-        board = get_board()
-
+        board = Board.get_board()
+        print(board)
         # returns False if there is a player occupying the [row][col] space or if the game has been won or drawn
-        if board[row][col] == player or self._current_state != "UNFINISHED":
+        if board[row][col] == player or Board.get_current_sate() != "UNFINISHED":
             return False
 
         # records the player's move at the [row][col] space
         board[row][col] = player
 
         # if the current player has 5 in a row, the game state is updated to <player>_WON
-        if is_winner(row, col, player):
-            set_current_state(f"{player.upper()}_WON")
+        if Board.is_winner(row, col, player):
+            Board.set_current_state(f"{player.upper()}_WON")
         # if there are no available moves left and neither player has won, the game state is changed to DRAW
-        elif is_draw() == 0:
-            set_current_state("DRAW")
+        elif Board.is_draw() == 0:
+            Board.set_current_state("DRAW")
 
         return True
