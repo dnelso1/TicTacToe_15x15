0d56d7c85950212e026c78f0ae774f305a999e65
Drew Nelson <drew.nelson72@gmail.com>
2023-01-11 15:22:07 -0500

    changed spacing in make_move function

--- TicTacToe_15x15.py
+++ TicTacToe_15x15.py
@@ -51,19 +51,22 @@ class FiveBoard:
     def make_move(self, row, col, player):
         """Makes a move based on the given [row][col] position and which player is making a turn, 'x' or 'o'"""
         board = self._board
-        self.print_board(board)
+        # self.print_board(board)
+
         # returns False if there is a player occupying the [row][col] space or if the game has been won or drawn
         if board[row][col] == 'x' or board[row][col] == 'o' or self._current_state != "UNFINISHED":
             return False
 
         # records the player's move at the [row][col] space
         board[row][col] = player
+
         # if the current player has 5 in a row, the game state is updated to <player>_WON
         if self.is_winner(player):
             self._current_state = f"{player.upper()}_WON"
         # if there are no available moves left and neither player has won, the game state is changed to DRAW
         elif self.is_draw() == 0:
             self._current_state = "DRAW"
+
         return True
 
     def is_winner(self, player):
