8605b25e3383a949ad6515a4cbc8381d4ae38c44
Drew Nelson <drew.nelson72@gmail.com>
2023-01-11 16:02:12 -0500

    made a set_current_state function

--- Board.py
+++ Board.py
@@ -48,6 +48,10 @@ class Board:
         """Returns the current state of the board"""
         return self._current_state
 
+    def set_current_state(self, state):
+        """Sets the current state of the board"""
+        self._current_state = state
+
     def make_move(self, row, col, player):
         """Makes a move based on the given [row][col] position and which player is making a turn, 'x' or 'o'"""
         board = self._board
@@ -61,10 +65,10 @@ class Board:
 
         # if the current player has 5 in a row, the game state is updated to <player>_WON
         if self.is_winner(row, col, player):
-            self._current_state = f"{player.upper()}_WON"
+            self.set_current_state(f"{player.upper()}_WON")
         # if there are no available moves left and neither player has won, the game state is changed to DRAW
         elif self.is_draw() == 0:
-            self._current_state = "DRAW"
+            self.set_current_state("DRAW")
 
         return True
 
