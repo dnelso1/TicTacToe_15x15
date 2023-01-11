45c303e27328f46fe7dff934590a3ee3be7b717f
Drew Nelson <drew.nelson72@gmail.com>
2023-01-11 18:35:01 -0500

    imported board_inst and added an __init__ method

--- Game.py
+++ Game.py
@@ -1,22 +1,26 @@
-from Board import Board
+from Board import Board, board_inst
 
 class Game:
+    def __init__(self) -> None:
+        self._player1 = ''
+        self._player2  = ''
+
     def make_move(self, row, col, player):
         """Makes a move based on the given [row][col] position and which player is making a turn, 'x' or 'o'"""
-        board = Board.get_board()
+        board = board_inst.get_board()
         
         # returns False if there is a player occupying the [row][col] space or if the game has been won or drawn
-        if board[row][col] == player or Board.get_current_sate() != "UNFINISHED":
+        if board[row][col] == player or board_inst.get_current_state() != "UNFINISHED":
             return False
 
         # records the player's move at the [row][col] space
         board[row][col] = player
 
         # if the current player has 5 in a row, the game state is updated to <player>_WON
-        if Board.is_winner(row, col, player):
-            Board.set_current_state(f"{player.upper()}_WON")
+        if board_inst.is_winner(row, col, player):
+            board_inst.set_current_state(f"{player.upper()}_WON")
         # if there are no available moves left and neither player has won, the game state is changed to DRAW
-        elif Board.is_draw() == 0:
-            Board.set_current_state("DRAW")
+        elif board_inst.is_draw() == 0:
+            board_inst.set_current_state("DRAW")
 
         return True
