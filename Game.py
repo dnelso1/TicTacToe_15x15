e3abca6bdf64269ad0898c4ebd5400fb9b7b9307
Drew Nelson <drew.nelson72@gmail.com>
2023-01-11 19:19:20 -0500

    added getters/setters for player1/2

--- Game.py
+++ Game.py
@@ -5,7 +5,19 @@ class Game:
         self._player1 = ''
         self._player2  = ''
 
-    def make_move(self, row, col, player):
+    def set_player1(self, icon)  -> None:
+        self._player1 = icon
+
+    def set_player2(self, icon) -> None:
+        self._player2 = icon
+
+    def get_player1(self) -> str:
+        return self._player1
+
+    def get_player2(self) -> str:
+        return self._player2
+
+    def make_move(self, row, col, player) -> bool:
         """Makes a move based on the given [row][col] position and which player is making a turn, 'x' or 'o'"""
         board = board_inst.get_board()
         
@@ -15,9 +27,8 @@ class Game:
 
         # records the player's move at the [row][col] space
         board[row][col] = player
-
         # if the current player has 5 in a row, the game state is updated to <player>_WON
-        if board_inst.is_winner(row, col, player):
+        if board_inst.is_winner(player):
             board_inst.set_current_state(f"{player.upper()}_WON")
         # if there are no available moves left and neither player has won, the game state is changed to DRAW
         elif board_inst.is_draw() == 0:
