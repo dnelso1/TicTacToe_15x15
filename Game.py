3586b3572cdb5225c422fc2a8e8c217e2879ecc7
Drew Nelson <drew.nelson72@gmail.com>
2023-01-12 10:34:43 -0500

    cleaned up the board display

--- Game.py
+++ Game.py
@@ -1,15 +1,16 @@
 from Board import Board, board_inst
-import os
-import time
+from os import system
+from time import sleep
 
 class Game:
-    def make_move(self, row, col, player) -> bool:
+    def make_move(self, row, col, player) -> None:
         """Makes a move based on the given [row][col] position and which player is making a turn, 'x' or 'o'"""
         board = board_inst.get_board()
         
         # returns False if there is a player occupying the [row][col] space or if the game has been won or drawn
-        if board[row][col] == player or board_inst.get_current_state() != "UNFINISHED":
-            return False
+        if board[row][col] != " ":
+            print("That space is not open! Try again.")
+            sleep(1.5)
 
         # records the player's move at the [row][col] space
         board[row][col] = player
@@ -19,25 +20,25 @@ class Game:
         # if there are no available moves left and neither player has won, the game state is changed to DRAW
         elif board_inst.is_draw() == 0:
             board_inst.set_current_state("DRAW")
-
-        return True
     
     def play_game(self) -> None:
         """Plays the game until it is finished"""
         while board_inst.get_current_state() == 'UNFINISHED':
-            os.system('clear')
+            system('clear')
             board_inst.print_board()
             player = input("Who is making a move? (x/o) > ")
+            system('clear')
+            board_inst.print_board()
             move = input(f"What is {player.upper()}'s move? (Enter a row,column coordinate: 1,4) > ")
             move = tuple(move.split(","))
             self.make_move(int(move[0]), int(move[1]), player.lower())
         
-        os.system('clear')
+        system('clear')
         if board_inst.get_current_state() == 'X_WON':
             print("X Wins!!!")
         elif board_inst.get_current_state() == 'O_WON':
             print("O Wins!!!")
         elif board_inst.get_current_state() == 'DRAW':
             print("Sadly, it was a DRAW")
-        time.sleep(5)
-        os.system('clear')
+        sleep(5)
+        system('clear')
